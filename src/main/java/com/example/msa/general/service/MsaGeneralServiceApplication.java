656d80eb0b451651afb136a4b938e85be351f8d3
glass <silver20190204@gmail.com>
2024-11-05 19:23:03 +0900

    feat: first setting

---
+++ src/main/java/com/example/msa/general/service/MsaGeneralServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.msa.general.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsaGeneralServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsaGeneralServiceApplication.class, args);
+	}
+
+}
