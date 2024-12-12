252c6d71fba98a44a5f0abf37385a94913d9e469
glass <silver20190204@gmail.com>
2024-12-13 02:32:35 +0900

    Add GeneralUserEntity for HashMap

---
+++ src/main/java/com/example/msa/general/service/general/adapter/out/hashmap/GeneralUserEntity.java
@@ -0,0 +1,16 @@
+package com.example.msa.general.service.general.adapter.out.hashmap;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * <b> HashMap을 위한 GeneralUser Entity </b>
+ */
+@Getter
+@Setter
+@AllArgsConstructor
+public class GeneralUserEntity {
+    private String email;
+    private int age;
+}
