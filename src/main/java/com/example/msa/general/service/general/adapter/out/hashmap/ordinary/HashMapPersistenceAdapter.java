a56054d81a42e384b36be148105c6c098c2a8b4d
glass <silver20190204@gmail.com>
2024-12-13 15:42:47 +0900

    Add GeneralUserDataOutputPort

--- src/main/java/com/example/msa/general/service/general/adapter/out/hashmap/ordinary/HashMapPersistenceAdapter.java
+++ src/main/java/com/example/msa/general/service/general/adapter/out/hashmap/ordinary/HashMapPersistenceAdapter.java
@@ -1,6 +1,7 @@
 package com.example.msa.general.service.general.adapter.out.hashmap.ordinary;
 
 import com.example.msa.general.service.general.adapter.out.hashmap.GeneralUserEntity;
+import com.example.msa.general.service.general.application.service.port.out.GeneralUserDataOutputPort;
 import org.springframework.stereotype.Component;
 
 import java.util.HashMap;
@@ -12,7 +13,7 @@ import java.util.Map;
  * - 데이터베이스 대신 해시맵을 활용한 어댑터
  */
 @Component
-public class HashMapPersistenceAdapter {
+public class HashMapPersistenceAdapter implements GeneralUserDataOutputPort {
     private final Map<String, GeneralUserEntity> users;
 
     HashMapPersistenceAdapter() {
@@ -30,6 +31,7 @@ public class HashMapPersistenceAdapter {
      * @param email 예시 데이터
      * @param age 예시 데이터
      */
+    @Override
     public void saveGeneralUser(String id, String email, int age) {
         users.put(id, new GeneralUserEntity(email, age));
         System.out.printf("저장된 데이터: 아이디 = %s, 이메일 = %s, 나이 = %d \n", id, email, age);
